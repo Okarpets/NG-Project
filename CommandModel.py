ba2c116332846fd8a33e18f5e566b7b486603252
Okarpets <karpetsoleksandrschool10@gmail.com>
2023-12-22 17:28:29 +0200

    Formatted the code for better readability

--- CommandModel.py
+++ CommandModel.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 import json
 
 
-#COMAND 0 - HELP
+#COMMAND USER : 0 - HELP (MAUNAL)
 def help():
     print(
     "This program was created as a website tester, below you can see a list of existing commands:\n\n" +
@@ -24,8 +24,7 @@ def help():
     )
     work()
 
-
-#COMMAND 1 - RETURN GET CODE REQUESTS
+#COMMAND USER : 1 - RETURN GET CODE REQUESTS (OR ANSWER AFTER PROCESS TYPICAL COMMANDS)
 def code_get(orderarray):
     p_url = orderarray[1]
     try:
@@ -81,8 +80,7 @@ def code_get(orderarray):
     lst.close()
     work()
 
-
-#COMMAND 2 - RETURN POST CODE REQUESTS
+#COMMAND USER : 2 - RETURN POST CODE REQUESTS
 def code_post(orderarray):
     try:
         url = orderarray[1]
@@ -118,17 +116,17 @@ def code_post(orderarray):
     work()
 
 
-#COMMAND 3 - ELEMENT BY ID
+#COMMAND USER : 3 - RETURN HTML ELEMENT BY ID
 def byid(p_url, p_id):
     url = p_url
     site = requests.get(url)
     soup = BeautifulSoup(site.text, 'html.parser')
     u_id = soup.find_all(id='{0}'.format(p_id))
     print(u_id)
-    if u_id != []:
-        u_id = "The element IS on the page"
-    else:
+    if u_id == []:
         u_id = "The element ISN'T on the page"
+    else:
+        u_id = "The element IS on the page"
     print('\t' + u_id + '\n')
     file = 'TestResults.xlsx' #Create or open file with that name
     lst = load_workbook(file) #It is depend on save and close command
@@ -139,28 +137,7 @@ def byid(p_url, p_id):
     work()
 
 
-#COMAND 4 - FORMATS .XLSX DOCUMENT
-def excel():
-    file = 'TestResults.xlsx'
-    lst = load_workbook(file) #It is depend on save and close command
-    for sheet_name in lst.sheetnames:
-        sheet = lst[(sheet_name)]
-        lst.remove(sheet)
-    lst.create_sheet('ScenarioResults')
-    lst.create_sheet('CodeGetResults') # CREATE LISTS
-    lst.create_sheet('CodePostResults')
-    lst.create_sheet('ElementById')
-    lst.create_sheet('ElementByTag')
-    lst['CodeGetResults'].append(["Url","Order"])
-    lst['CodePostResults'].append(["Url","Order"])
-    lst['ElementById'].append(["Given Id","Url","Result"])
-    lst['ElementByTag'].append(["Given Tag","Url","Result"])
-    lst['ScenarioResults'].append(["Scenario ID","Url","Get requests", "Post requests", "Find-order by id","Find-order by tag", "Scenario-file name"])
-    lst.save(file) #REMEMBER THE WRITING RULES
-    lst.close()
-
-    
-#COMMAND 5 - ELEMENT BY TAG
+#COMMAND USER : 4 - RETURN HTML ELEMENT BY TAG
 def bytag(p_url, p_tag):
     url = p_url
     site = requests.get(url)
@@ -168,8 +145,8 @@ def bytag(p_url, p_tag):
     u_tag = soup.find_all('{0}'.format(p_tag))
     if u_tag == []:
         u_tag = "Tag ISN'T HERE"
-    print('\t' + str(u_tag) + '\n')
-    if u_tag != []:
+        print('\t' + str(u_tag) + '\n')
+    else:
         u_tag = "Tag IS here"
     file = 'TestResults.xlsx' #Create or open file with that name
     lst = load_workbook(file) #It is depend on save and close command
@@ -179,8 +156,7 @@ def bytag(p_url, p_tag):
     lst.close()
     work()
 
-
-#COMMAND 6 - CREATE SCENARIO MODEL
+#COMMAND USER : 5 - CREATE SCENARIO TO JSON FILE
 def create(p_name):
     file = open(f"{p_name}.json", 'w')
     file.write('[''\n')
@@ -211,40 +187,30 @@ def create(p_name):
         while oper != "end":
             match under_create(): # Getting the operation from the request
                 case "1":
-
                     par_order = str(input("\tDo you want to add params? (Y/ )\n"))
                     if par_order == 'Y' or par_order == 'y':
                         get_params = str(input("\tEnter name of the json-file with params\n"))
                         get = "2"
                     else:
                         get = "1"
-
-                case "2":
-                                    
+                case "2":       
                     par_order = str(input("\tDo you want to add data? (Y/ )\n"))
                     if par_order == 'Y' or par_order == 'y':
                         post_data = str(input("\tEnter name of the json-file with data\n"))
                         post = "2"
                     else:
                         post = "1"
-
-                case "3":
-                                            
-                    htmlid = str(input("\tEnter HTML elemnent id\n"))
-                        
-                case "4":
-                                            
+                case "3":                   
+                    htmlid = str(input("\tEnter HTML elemnent id\n"))                       
+                case "4":                            
                     htmltag = str(input("\tEnter HTML elemnent tag\n"))
-
                 case "E":
-
                     json.dump(data, file)
                     file.write('\n'']')
                     file.close()
                     oper = "end"      
                     oper_endp = "end"
                 case "C":
-
                     if get == "0" and post == "0" and htmlid == "0" and htmltag == "0":
                         print("The scenario will not be added because it is empty")
                     else:
@@ -252,16 +218,32 @@ def create(p_name):
                         file.write(',\n')
                         scen_id += 1
                         oper = "end"
-                
                 case _:
                     print("This command doesn't exist, use \"Command list\"")
-                    
             data = {"id" : scen_id, "url" : url, "get" : get, "params" : get_params, "post" : post, "data" : post_data, "htmlid" : htmlid, "htmltag" : htmltag}
         if oper_endp == "end":
             break
             
+#COMMAND USER : 6 - COMMAND TO CREATE REQUESTS FOR SCENARIO PROCESSING IN AN EXCEL FILE
+def read(p_name, p_id):
+    if p_id == "0":
+        file = open("{0}.json".format(p_name), "r")
+        file = json.load(file) 
+        for lines in file:
+            handle(lines, p_name)
+        print("\n\t\tAll the scenario was processed\n")
+    else:  
+        with open("{0}.json".format(p_name), "r") as file:
+            js_file = json.load(file)
+            file.close()
+            for elem in js_file:
+                if str(elem['id']) == str(p_id):
+                    handle(elem, p_name)
+        print("\n\tScenario was processed\n")
+    work()
 
-#COMAND 7 - SHOW ALL THE FILE OF SCENARIO
+
+#COMMAND USER : 7 - SHOW ALL THE FILE OF SCENARIO
 def show(p_name, p_id):
     with open("{0}.json".format(p_name), "r") as file:
         if p_id == "0":
@@ -281,7 +263,37 @@ def show(p_name, p_id):
         work()
 
 
-#COMMAND 8 - READ SCENARIO MODEL
+#TECHNICAL COMMANDS NOT USER APPLICABLE
+############################################################################
+    
+#COMMAND ANSWER : 1 - FORMATS .XLSX DOCUMENT
+def excel():
+    file = 'TestResults.xlsx'
+    lst = load_workbook(file) #It is depend on save and close command
+    for sheet_name in lst.sheetnames:
+        sheet = lst[(sheet_name)]
+        lst.remove(sheet)
+    lst.create_sheet('ScenarioResults')
+    lst.create_sheet('CodeGetResults') # CREATE LISTS
+    lst.create_sheet('CodePostResults')
+    lst.create_sheet('ElementById')
+    lst.create_sheet('ElementByTag')
+    lst['CodeGetResults'].append(["Url","Order"])
+    lst['CodePostResults'].append(["Url","Order"])
+    lst['ElementById'].append(["Given Id","Url","Result"])
+    lst['ElementByTag'].append(["Given Tag","Url","Result"])
+    lst['ScenarioResults'].append(["Scenario ID","Url","Get requests", "Post requests", "Find-order by id","Find-order by tag", "Scenario-file name"])
+    lst.save(file) #REMEMBER THE WRITING RULES
+    lst.close()
+
+
+#COMMAND ANSWER : 2 - ERROR MESSAGES
+def err():
+    print("\t\tInvalid value\n\tTry again or use -help for help\n")
+    work()
+
+
+#COMMAND ANSWER : 3 - PROCESS SCENARIOS
 def handle(lines, p_name):
         scen_id = lines['id']
         url = lines['url']
@@ -292,6 +304,7 @@ def handle(lines, p_name):
         u_id = lines['htmlid']
         u_tag = lines['htmltag']
 
+        #GET PART
         if code_get == "1":
             site = requests.get(url)
             get_order = site.status_code
@@ -306,7 +319,7 @@ def handle(lines, p_name):
         else:
             get_order = "The operation was not requested"
 
-
+        #POST PART
         if code_post == "1":
             site = requests.post(url)
             post_order = site.status_code
@@ -320,8 +333,8 @@ def handle(lines, p_name):
                 post_order = "Uncorrectly requests"
         else:
             post_order = "The operation was not requested"
-        
 
+        #ID PART
         if u_id != "0":
             site = requests.get(url)
             soup = BeautifulSoup(site.text, 'html.parser')
@@ -333,7 +346,7 @@ def handle(lines, p_name):
         else:
             u_id = "The operation was not requested"
         
-
+        #TAG PART
         if u_tag != "0":
             print(u_tag)
             site = requests.get(url)
@@ -352,30 +365,8 @@ def handle(lines, p_name):
         xlsx.append([scen_id,url,get_order,post_order,u_id,u_tag,p_name]) #Adding that data in xlsx file
         lst.save(file) #REMEMBER THE WRITING RULES
         lst.close()
-
-
-def read(p_name, p_id):
-    if p_id == "0":
-        file = open("{0}.json".format(p_name), "r")
-        file = json.load(file) 
-        for lines in file:
-            handle(lines, p_name)
-        print("\n\t\tAll the scenario was processed\n")
-    else:  
-        with open("{0}.json".format(p_name), "r") as file:
-            js_file = json.load(file)
-            file.close()
-            for elem in js_file:
-                if str(elem['id']) == str(p_id):
-                    handle(elem, p_name)
-        print("\n\tScenario was processed\n")
-    work()
-
-#COMMAND ANSWER - ERROR MESSAGES
-def err():
-    print("\t\tInvalid value\n\tTry again or use -help for help\n")
-    work()
-
+############################################################################
+    
 
 #COMMAND MAIN - START 
 #STARTS A FUNCTION TREE
@@ -383,11 +374,14 @@ def work():
     order = str(input('\n'))
     orderarray = order.split(" ") # Вreaking the request into a command and a URL
     match orderarray[0]: # Getting the operation from the request
-        case "-help":
+        case"-help":
                 try:
                     help()
                 except Exception:
                     err()
+        case"-exit":
+                print("\t\tExit the program")
+                return 0
         case"-code_get":
                 code_get(orderarray)
         case"-code_post":
@@ -412,17 +406,6 @@ def work():
                     create(p_name)
                 except Exception:
                     err()
-        case"-show":
-                try:
-                    if len(orderarray) == 2:
-                        p_name = orderarray[1]
-                        show(p_name, "0")
-                    if len(orderarray) == 3:
-                        p_name = orderarray[1]
-                        p_id = orderarray[2]
-                        show(p_name, p_id)
-                except Exception:
-                    err()
         case"-read":
                 try:
                     if len(orderarray) == 2:
@@ -434,9 +417,17 @@ def work():
                         read(p_name, p_id)
                 except Exception:
                     err()
-        case"-exit":
-                    print("Exit the program")
-                    return 0
+        case"-show":
+                try:
+                    if len(orderarray) == 2:
+                        p_name = orderarray[1]
+                        show(p_name, "0")
+                    if len(orderarray) == 3:
+                        p_name = orderarray[1]
+                        p_id = orderarray[2]
+                        show(p_name, p_id)
+                except Exception:
+                    err()
         case _:
                 print("\tError\n" + "\tWe're sorry, but this command doesn't exist, please use -help")
                 work()
